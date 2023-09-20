73e4e83975cb831017bdcb410677e4aa84bbfe52
Sagar Pant <sagarpant1997@gmail.com>
2023-09-20 09:14:17 +0530

    new commit

--- src/components/MovingBox.jsx
+++ src/components/MovingBox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Moving.module.css'
 import { useMeasure } from "@uidotdev/usehooks";
 
